c6cb2a854ed0abfaf8981c0ec1926880fe28ec36
omarelsayed <omar.elsayed.soliman@gmail.com>
2018-10-26 15:54:53 +0200

    fix_enable_bootReceiver

--- app/src/main/java/com/example/omar/cs193a/SettingsFragment.java
+++ app/src/main/java/com/example/omar/cs193a/SettingsFragment.java
@@ -1,29 +1,35 @@
 package com.example.omar.cs193a;
 
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     SharedPreferences.OnSharedPreferenceChangeListener listener;
+    Activity parent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
         listener = this;
+    }
 
-
-
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        parent = getActivity();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+
     }
 
     @Override
@@ -35,17 +41,11 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         String bootKey = getResources().getString(R.string.switch_preference_boot);
-        if(key.equals(bootKey)) {
-            if(sharedPreferences.getBoolean(bootKey, false)) {
-                getActivity().registerReceiver(new BootReceiver(), new IntentFilter(Intent.ACTION_BOOT_COMPLETED));
-            }
-            else {
-                try {
-                    getActivity().unregisterReceiver(new BootReceiver());
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
+        if (key.equals(bootKey)) {
+            if (sharedPreferences.getBoolean(bootKey, false)) {
+                parent.getPackageManager().setComponentEnabledSetting(new ComponentName(parent, BootReceiver.class), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+            } else {
+                parent.getPackageManager().setComponentEnabledSetting(new ComponentName(parent, BootReceiver.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
             }
         }
     }
